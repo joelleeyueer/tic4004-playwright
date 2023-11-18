2a8ea96d3f9a4c5498f4bfabf20c99e6791664e4
joelleeyueer <35606477+joelleeyueer@users.noreply.github.com>
2023-11-18 19:23:27 +0800

    Removed example test
    Use playwright modules instead of node (test with npx instead of node, use test and expect instead of try catch)

--- tests/purchaseItem-test.spec.ts
+++ tests/purchaseItem-test.spec.ts
@@ -1,66 +1,48 @@
-const { firefox } = require('playwright');
+import { test, expect } from '@playwright/test';
 
-(async () => {
-  const browser = await firefox.launch({ headless: false });
+test('can purchase and checkout', async ({ browser }) => {
   const page = await browser.newPage();
 
-  try {
-    // Go to login page
-    await page.goto('https://www.saucedemo.com/');
+  // Go to login page
+  await page.goto('https://www.saucedemo.com/');
 
-    // Type in id and pw
-    await page.fill('#user-name', 'standard_user');
-    await page.fill('#password', 'secret_sauce');
-    await page.press('#password', 'Enter');
+  // Type in id and pw
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.press('#password', 'Enter');
 
-    // Assert the URL is the inventory page
-    await page.waitForURL('https://www.saucedemo.com/inventory.html');
+  // Expects /inventory
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
-    // Add an item to the cart
-    await page.click('#add-to-cart-sauce-labs-backpack');
+  // Add an item to the cart
+  await page.click('#add-to-cart-sauce-labs-backpack');
 
-    // Assert that the cart has 1 item
-    await page.waitForSelector('.shopping_cart_badge');
-    const itemsInCart = await page.textContent('.shopping_cart_badge');
-    if (itemsInCart !== '1') {
-      throw new Error(`Expected 1 item in cart, found ${itemsInCart}`);
-    }
+  // Expect that the cart has 1 item
+  await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
 
-    // Navigate to the cart page
-    await page.click('.shopping_cart_link');
-    await page.waitForURL('https://www.saucedemo.com/cart.html');
+  // Go to cart page
+  await page.click('.shopping_cart_link');
+  await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
 
-    // Assert that the correct item is added
-    const itemAdded = await page.isVisible('text=Sauce Labs Backpack');
-    if (!itemAdded) {
-      throw new Error('The item was not added to the cart.');
-    }
+  // Expect that the backpack is added
+  await expect(page.locator('text=Sauce Labs Backpack')).toBeVisible();
 
-    // Proceed to checkout
-    await page.click('text=Checkout');
+  // Go to checkout
+  await page.click('text=Checkout');
 
-    // Fill in the checkout information
-    await page.fill('#first-name', 'Joel');
-    await page.fill('#last-name', 'Lee');
-    await page.fill('#postal-code', '642684');
+  // Fill in the checkout information and continue
+  await page.fill('#first-name', 'Joel');
+  await page.fill('#last-name', 'Lee');
+  await page.fill('#postal-code', '642684');
+  await page.click('#continue');
 
-    // Click continue
-    await page.click('#continue');
+  // Expect checkout step two page, and click finish
+  await expect(page).toHaveURL('https://www.saucedemo.com/checkout-step-two.html');
+  await page.click('#finish');
 
-    // Click finish on the next page
-    await page.waitForURL('https://www.saucedemo.com/checkout-step-two.html');
-    await page.click('#finish');
+  // Expect checkout complete page
+  await expect(page).toHaveURL('https://www.saucedemo.com/checkout-complete.html');
 
-    // Assert that the checkout is complete
-    await page.waitForURL('https://www.saucedemo.com/checkout-complete.html');
-    const header = await page.textContent('.complete-header');
-    if (header !== 'Thank you for your order!') {
-      throw new Error('The thank you message did not appear as expected.');
-    }
-    console.log('TC3 Purchase Item Test completed successfully.');
-  } catch (error) {
-    console.error('TC3 Purchase Item Test failed:', error.message);
-  } finally {
-    await browser.close();
-  }
-})();
+  // Assert the "Thank you for your order!" text is present
+  await expect(page.locator('.complete-header')).toHaveText('Thank you for your order!');
+});
