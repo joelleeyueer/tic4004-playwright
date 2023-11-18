16f994f06561cc4faa3071e1f2615a3e1f708e12
joelleeyueer <35606477+joelleeyueer@users.noreply.github.com>
2023-11-18 23:47:57 +0800

    Update TC4-emptyCart-test.spec.ts

--- tests/TC4-emptyCart-test.spec.ts
+++ tests/TC4-emptyCart-test.spec.ts
@@ -20,29 +20,10 @@ test('can purchase and checkout', async ({ browser }) => {
   // Expect that the cart has 1 item
   await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
 
-  // Go to cart page
+  // Go to cart page and remove item
   await page.click('.shopping_cart_link');
-  await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
+  await page.click('#remove-sauce-labs-backpack');
+  await page.click('.shopping_cart_link');
+  await expect(page).not.toHaveURL('https://www.saucedemo.com/cart.html');
 
-  // Expect that the backpack is added
-  await expect(page.locator('text=Sauce Labs Backpack')).toBeVisible();
-
-  // Go to checkout
-  await page.click('text=Checkout');
-
-  // Fill in the checkout information and continue
-  await page.fill('#first-name', 'Joel');
-  await page.fill('#last-name', 'Lee');
-  await page.fill('#postal-code', '642684');
-  await page.click('#continue');
-
-  // Expect checkout step two page, and click finish
-  await expect(page).toHaveURL('https://www.saucedemo.com/checkout-step-two.html');
-  await page.click('#finish');
-
-  // Expect checkout complete page
-  await expect(page).toHaveURL('https://www.saucedemo.com/checkout-complete.html');
-
-  // Assert the "Thank you for your order!" text is present
-  await expect(page.locator('.complete-header')).toHaveText('Thank you for your order!');
 });
